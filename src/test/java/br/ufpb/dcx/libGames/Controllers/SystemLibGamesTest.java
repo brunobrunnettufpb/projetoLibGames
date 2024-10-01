3df7c303e45c41650a9406b9b4591f1edb4a2517
Bruno Brunnett <bruno.mangueira@dcx.ufpb.br>
2024-10-01 09:07:54 -0300

    TODOs test

---
+++ src/test/java/br/ufpb/dcx/libGames/Controllers/SystemLibGamesTest.java
@@ -0,0 +1,27 @@
+package br.ufpb.dcx.libGames.Controllers;
+
+import org.junit.jupiter.api.Test;
+
+public class SystemLibGamesTest {
+    @Test
+    public void TestarCadadastroUsuario() {
+        // TODO: Cadastrar usuário.
+    }
+
+    @Test
+    public void TestarCompraDeJogo() {
+        // TODO: Compra de jogo.
+    }
+
+    @Test
+    public void TestarPersistencia() {
+        // TODO: Cadastrar usuário, comprar jogo e persistir os dados.
+    }
+
+    @Test
+    public void TestarPersistenciaRecuperar() {
+        // TODO: Recuperar os dados e verificar usuários cadastrados na função anterior.
+        // TODO: Verificar a quantidades de jogos inserido na lista do usuário.
+        // TODO: Verificar saldo do usuário.
+    }
+}

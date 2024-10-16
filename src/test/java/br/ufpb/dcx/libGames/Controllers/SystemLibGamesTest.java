6b63b23edad81ebf4078e4797b7eed04c0468ded
EvilLink <evillink@live.com>
2024-10-16 15:31:26 -0300

    tests

--- src/test/java/br/ufpb/dcx/libGames/Controllers/SystemLibGamesTest.java
+++ src/test/java/br/ufpb/dcx/libGames/Controllers/SystemLibGamesTest.java
@@ -2,6 +2,8 @@ package br.ufpb.dcx.libGames.Controllers;
 
 import br.ufpb.dcx.libGames.Models.Game;
 import br.ufpb.dcx.libGames.Models.User;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
@@ -10,63 +12,57 @@ import static org.junit.jupiter.api.Assertions.*;
 
 
 public class SystemLibGamesTest {
-    @Test
-    public void TestarCadadastroUsuario() {
+    SystemLibGames sistema = new SystemLibGames();
+
+    @BeforeAll
+    @AfterAll
+    static void ExcluiArquivo() {
         File file = new File(GravadorDeDadosController.USERSFILE);
         if (file.exists()) file.delete();
+    }
 
-        SystemLibGames sistema = new SystemLibGames();
-
-        assertEquals(3, sistema.getQtdGames()); // Verifica se tem 3 jogos
+    @Test
+    void TestarCadadastroUsuario() {
         try {
-            sistema.userCreate("João", "jo22", 50);
-            assertEquals(1, sistema.getQtdUsers()); // Checa se cadastrou João
-            User jo = sistema.getUser("jo22");
+            sistema.userCreate("jo22", "João", 50); // Cadastra no sistema
+            User jo = sistema.getUser("jo22"); // Pega para checar se cadastrou correto
             assertEquals("João", jo.getName()); // Checa se o nome é João
             assertEquals("jo22", jo.getUsername()); // Checa se o username é jo22
             assertEquals(50, jo.getBalance().getValue()); // Checa se o saldo é R$50
 
-            sistema.userCreate("Maria", "mar80", 100);
-            User maria = sistema.getUser("mar80");
-            assertEquals(1, sistema.getQtdUsers()); // Checa se cadastrou Maria
+            sistema.userCreate("mar80", "Maria", 100); // Cadastra no sistema
+            User maria = sistema.getUser("mar80"); // Pega para checar se cadastrou correto
             assertEquals("Maria", maria.getName()); // Checa se o nome é Maria
             assertEquals("mar80", maria.getUsername()); // Checa se o username é mar80
             assertEquals(100, maria.getBalance().getValue()); // Checa se o saldo é R$100
         }
         catch (Exception ex) {
-
         }
-        file.delete();
     }
 
     @Test
-    public void TestarCompraDeJogo() {
-        // TODO: Compra de jogo.
-        File file = new File(GravadorDeDadosController.USERSFILE);
-        if (file.exists()) file.delete();
+    void TestarBuscarUsuario() {
+        assertEquals(2, sistema.getQtdUsers()); // Checa se cadastrou os 2 no teste anterior.
+    }
 
-        SystemLibGames sistema = new SystemLibGames();
+    @Test
+    void TestaCadastrarJogos() {
+        assertEquals(3, sistema.getQtdGames()); // Verifica se tem os 3 jogos cadastrados no sistema.
+    }
 
+    @Test
+    void TestarCompraDeJogo() {
         try {
-            sistema.userCreate("João", "jo22", 50);
             User joao = sistema.getUser("jo22");
+            assertTrue(joao.getGames().isEmpty()); // Verifica que não tem nenhum jogo comprado.
+
             Game game = sistema.getGame("Witcher 3");
             sistema.gameBuy(joao, game);
-            joao = sistema.getUser("jo22"); // Atualiza pra poder verificar
-            assertNull(joao.getGame("Witcher 3"));
+            joao = sistema.getUser("jo22"); // Atualiza pra poder verificar a compra.
+
+            assertEquals(1, joao.getGames().size());
         }
         catch (Exception ex) {
-
         }
-
-
-        file.delete();
-    }
-
-    @Test
-    public void TestarPersistenciaRecuperar() {
-        // TODO: Recuperar os dados e verificar usuários cadastrados na função anterior.
-        // TODO: Verificar a quantidades de jogos inserido na lista do usuário.
-        // TODO: Verificar saldo do usuário.
     }
 }
